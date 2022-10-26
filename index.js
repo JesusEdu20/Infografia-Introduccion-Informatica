e0bb5089c396e2139501399c958f4d522a75e2eb
Jesus Eduardp <eduardoCastillo180@hotmail.com>
2022-10-25 21:58:07 -0400

    menu position added

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ let binaryToString= new binaryToStringObject;
 let arrayBinary=[]
 
 //recuerda base 32
-const ASCII=[{name:" "},{name:"!"},{name:""},{name:"#"},{name:"$"},{name:"&"},{name:""},{name:"("},{name:")"},{name:"*"},{name:"+"},{name:""},{name:"-"},{name:"."},{name:"/"},{name:"0"},{name:"1"},{name:"2"},{name:"3"},{name:"4"},{name:"5"},{name:"6"},{name:"7"},{name:"8"},{name:"9"},{name:":"},{name:";"},{name:"<"},{name:"="},{name:">"},{name:"?"},{name:"@"},{name:"A", value:97},{name:"B", value:98},{name:"C", value:98},{name:"D", value:98}, {name:"E", value:98}, {name:"F", value:98}, {name:"G", value:98}, {name:"H", value:98}, {name:"I", value:98}, {name:"J", value:98}, {name:"K", value:98}, {name:"L", value:98}, {name:"M", value:98}, {name:"N", value:98}, {name:"O", value:98}, {name:"P", value:98}, {name:"Q", value:98}, {name:"R", value:98}, {name:"S", value:98}, {name:"T", value:98}, {name:"U", value:98}, {name:"V", value:98}, {name:"W", value:98}, {name:"X", value:98}, {name:"Y", value:98}, {name:"Z", value:98},{name:"["},{name:""},{name:"]"},{name:"ʌ"},{name:"_"},{name:""},{name:"a", value:97},{name:"b", value:98},{name:"c", value:98},{name:"d", value:98}, {name:"e", value:98}, {name:"f", value:98}, {name:"g", value:98}, {name:"h", value:98}, {name:"i", value:98}, {name:"j", value:98}, {name:"k", value:98}, {name:"l", value:98}, {name:"m", value:98}, {name:"n", value:98}, {name:"o", value:98}, {name:"p", value:98}, {name:"q", value:98}, {name:"r", value:98}, {name:"s", value:98}, {name:"t", value:98}, {name:"u", value:98}, {name:"v", value:98}, {name:"w", value:98}, {name:"x", value:98}, {name:"y", value:98}, {name:"z", value:98},{name:"{"},{name:"|"}, {name:"}"}]
+const ASCII=[{name:" "},{name:"!"},{name:""},{name:"#"},{name:"$"},{name:"%"},{name:"&"},{name:""},{name:"("},{name:")"},{name:"*"},{name:"+"},{name:","},{name:"-"},{name:"."},{name:"/"},{name:"0"},{name:"1"},{name:"2"},{name:"3"},{name:"4"},{name:"5"},{name:"6"},{name:"7"},{name:"8"},{name:"9"},{name:":"},{name:";"},{name:"<"},{name:"="},{name:">"},{name:"?"},{name:"@"},{name:"A", value:97},{name:"B", value:98},{name:"C", value:98},{name:"D", value:98}, {name:"E", value:98}, {name:"F", value:98}, {name:"G", value:98}, {name:"H", value:98}, {name:"I", value:98}, {name:"J", value:98}, {name:"K", value:98}, {name:"L", value:98}, {name:"M", value:98}, {name:"N", value:98}, {name:"O", value:98}, {name:"P", value:98}, {name:"Q", value:98}, {name:"R", value:98}, {name:"S", value:98}, {name:"T", value:98}, {name:"U", value:98}, {name:"V", value:98}, {name:"W", value:98}, {name:"X", value:98}, {name:"Y", value:98}, {name:"Z", value:98},{name:"["},{name:""},{name:"]"},{name:"ʌ"},{name:"_"},{name:""},{name:"a", value:97},{name:"b", value:98},{name:"c", value:98},{name:"d", value:98}, {name:"e", value:98}, {name:"f", value:98}, {name:"g", value:98}, {name:"h", value:98}, {name:"i", value:98}, {name:"j", value:98}, {name:"k", value:98}, {name:"l", value:98}, {name:"m", value:98}, {name:"n", value:98}, {name:"o", value:98}, {name:"p", value:98}, {name:"q", value:98}, {name:"r", value:98}, {name:"s", value:98}, {name:"t", value:98}, {name:"u", value:98}, {name:"v", value:98}, {name:"w", value:98}, {name:"x", value:98}, {name:"y", value:98}, {name:"z", value:98},{name:"{"},{name:"|"}, {name:"}"}]
 
 /*Variables from DOM*/
 
@@ -27,19 +27,14 @@ const ASCII=[{name:" "},{name:"!"},{name:""},{name:"#"},{name:"$"},{name:"&"},{n
 
  /*Oyente*/
  sendBox.addEventListener("click", ()=>{const message=writeBox.value;
-    //let binaryMessageTraslted;
-    //let binaryMessage=[]
+ 
     let messageArray=message.split("");
     
-    //let texto;
    //TRANSLATOR;
    const binaryMessageTraslted= translator.traslate(messageArray, 32, ASCII, binary, binaryToString)
    
-
     send.lookElement(message,screen)
-    //let heightMessage=document.querySelector(".message-input")
-    /*console.log(heightMessage)
-    let height=heightMessage.clientHeight*/
+    
     send.lookElement(binaryMessageTraslted,responseScreen,true)
     /*test.scroll({top:0, behavior: "smooth"})*/
          
